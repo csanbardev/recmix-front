70ce737a8040f76eb98e12567830683e21cd54e3
csanbardev <csanbar97@gmail.com>
2026-09-16 13:38:59 +0200

    best ingredients styles

--- src/pages/Ingredients/Ingredients.jsx
+++ src/pages/Ingredients/Ingredients.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Box, Container, Heading, SimpleGrid, Text } from "@chakra-ui/react"
 import { useParams } from "react-router-dom"
 
 export function Ingredients() {
@@ -18,7 +19,6 @@ export function Ingredients() {
 
         const jsonData = await res.json();
 
-        console.log(jsonData)
         setData(jsonData);
       } catch (error) {
         window.alert(error.message)
@@ -28,15 +28,39 @@ export function Ingredients() {
   },[recId])
 
   return (
-    <section>
-      <ul>
+    <Box as="main" flex="1" bg="gray.950" py={{ base: 10, md: 16 }}>
+      <Container maxW="6xl" px={{ base: 5, md: 8 }}>
+        <Heading size="xl" color="gray.100" mb={8}>Ingredientes de la receta</Heading>
 
-        {
-          data?.map((ingredient) => (
-            <li key={ingredient.ire_ing_id}>{ingredient.ing_name} - Cantidad: {ingredient.ire_quantity}.</li>
-          ))
-        }
-      </ul>
-    </section>
+        <SimpleGrid
+          as="ul"
+          columns={{ base: 1, md: 2 }}
+          gap={4}
+          listStyleType="none"
+          m={0}
+          p={0}
+        >
+          {data?.map((ingredient) => (
+            <Box
+              as="li"
+              key={ingredient.ire_ing_id}
+              p={5}
+              borderWidth="1px"
+              borderColor="gray.700"
+              borderRadius="lg"
+              bg="gray.900"
+              boxShadow="sm"
+            >
+              <Text color="gray.100" fontSize="lg" fontWeight="semibold">
+                {ingredient.ing_name}
+              </Text>
+              <Text color="gray.400" mt={1}>
+                Cantidad: {ingredient.ire_quantity}
+              </Text>
+            </Box>
+          ))}
+        </SimpleGrid>
+      </Container>
+    </Box>
   )
 }
